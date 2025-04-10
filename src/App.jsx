a734fb9bd9ff10f102819abec582ed0ae9938f14
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-10 09:32:24 +0600

    index

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>All countris in the world...</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Countris/Countris.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countris = () => {
+    return (
+        
+        <div>
+
+        </div>
+    );
+};
+
+export default Countris;
